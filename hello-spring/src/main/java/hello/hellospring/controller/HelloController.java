9950c43ef53aecef78508e51e248d164089906c4
Heyrin <77548431+devheyrin@users.noreply.github.com>
2021-12-02 01:19:08 +0900

    Feat: "리턴 방법 비교 코드 추가"
    1. 정적 컨텐츠(resource/static)
    2. MVC와 템플릿 엔진(resource/templates)
    3. API(JSON 형태)

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,8 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -10,6 +12,38 @@ public class HelloController {
     public String hello(Model model){
         model.addAttribute("data", "hello!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(name="name", required = false) String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-mvc";
+    }
+
+    @GetMapping("hello-api-string")
+    @ResponseBody
+    public String helloApiString(@RequestParam(name="name", required = false) String name){
+        return "안녕하세요, " + name + "님!";
+    }
+
+    @GetMapping("hello-api-object")
+    @ResponseBody
+    public Hello helloApiObject(@RequestParam(name="name", required = false) String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 
 }
